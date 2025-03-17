1f9985e13f9958990450acd2d7d6aa5f7a0ccdf2
JunaidJamshid123 <jamshidjunaid763@gmail.com>
2025-03-17 22:29:38 +0500

    CategorizedArticles Model Modified

--- backend/models/CategorizedArticle.js
+++ backend/models/CategorizedArticle.js
@@ -1,14 +1,12 @@
-
-
 const mongoose = require("mongoose");
 
-const CategorizedArticleSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    summary: { type: String, required: true },
-    articles: [{ type: mongoose.Schema.Types.ObjectId, ref: "ScrapedArticle" }], // References Scraped Articles
-    background: [{ type: mongoose.Schema.Types.ObjectId, ref: "CategorizedArticle" }], // References other Categorized Articles
-    newsAnalytics: { type: String }, // Can be JSON formatted
-}, { timestamps: true });
+const CategorizedArticleSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true, trim: true, index: true },
+        summary: { type: String, required: true, trim: true },
+        articles: [{ type: mongoose.Schema.Types.ObjectId, ref: "ScrapedArticle", default: [] }], // References Scraped Articles
+    },
+    { timestamps: true, collection: "categorizedarticles"}
+);
 
-module.exports = mongoose.model("CategorizedArticle", CategorizedArticleSchema);
-
+module.exports = mongoose.model("categorizedarticles", CategorizedArticleSchema);
